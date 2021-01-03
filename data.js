05b59900d03dddd7bdb2465aae0e66ad1b3c2aea
marcelo alejnadro herlan <alelabestiadekiev@gmail.com>
2021-01-03 16:36:18 +0200

    git push -u origin main

---
+++ data.js
@@ -0,0 +1,11 @@
+const data=[{
+      url:'https://v58.tiktokcdn.com/video/tos/alisg/tos-alisg-pve-0037c001/985f08fd5fbc4ac7b8bcf1741aa10ab8/?VExpiration=1609654820&VSignature=T_-xjuJZ5OzExY9p2TaRKw&a=1233&br=2320&bt=1160&cd=0%7C0%7C1&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=2021010300193901019021807054AEC393&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ank2O2h0NGlqeTMzNzczM0ApOWRnZGQzNWVlN2hnaDZnN2dpZGJsXmFyZmRfLS1fMTRzcy4uLS0tXl82YjYwX2MyNF86Yw%3D%3D&vl=&vr=',
+      channel:'lobo',
+      description:'rabota',
+      song:'vamos las bandas',
+      likes:'123',
+      messages:'15',
+      shares:'52',
+}]
+
+export default data
